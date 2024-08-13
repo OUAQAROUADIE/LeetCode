522ef8ec676db844a10bea696688c02a3bfad7e2
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-08-13 13:19:36 +0100

    Time: 0 ms (100%), Space: 44 MB (91.78%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,37 +15,39 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-       if(root == null){
+          
+    if(root == null){
         return true;
-       }
-    return verifier(root) !=0;
     }
-    public int verifier(TreeNode node ){
+
+    return verifier(root) !=0;
+
+    }
+
+    public int verifier(TreeNode node){
         if(node == null){
             return 1;
         }
 
         int left = verifier(node.left);
-        if(left == 0){
+        if(left == 0 ){
             return 0;
         }
 
-        int right  = verifier(node.right);
-        if(right == 0){
+        int right = verifier(node.right);
+        if(right == 0 ){
             return 0;
         }
 
         if(Math.abs(left-right) > 1){
             return 0;
         }
-
         return Math.max(left, right)+1;
-
-
-
-
-
     }
+
+
+
+    
 
 
     
