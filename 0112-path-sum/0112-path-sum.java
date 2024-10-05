bdc87ce6f458a18f2f274387ea6a6170f927e539
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-10-05 12:22:45 +0100

    Time: 0 ms (100%), Space: 42.3 MB (99.74%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -15,17 +15,17 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root == null){
-            return false;
-        }
+      if( root == null ){
+        return false;
+      }
 
-        if(root.left == null && root.right == null){
-            return targetSum == root.val;
-        }
+      if(root.left == null && root.right == null){
+        return targetSum == root.val;
+      }
 
-        int newtargetSum = targetSum - root.val;
-
-        return hasPathSum(root.left, newtargetSum) || hasPathSum(root.right, newtargetSum );
+      int newtarget = targetSum - root.val;
+      return hasPathSum(root.left, newtarget) || hasPathSum(root.right, newtarget);
 
     }
+
 }
