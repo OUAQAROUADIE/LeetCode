e36f83867fb03ed920998a1ad6d372f442460e96
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-10-06 14:23:57 +0100

    Time: 2 ms (52.87%), Space: 41.5 MB (52.85%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,24 +1,26 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> pile = new Stack<>();
+    Stack<Character> pile = new Stack<>();
+    HashMap<Character, Character> map = new HashMap<>();
 
-       HashMap<Character, Character> map = new HashMap<>();
-       map.put(')', '(');
-       map.put(']', '[');
-       map.put('}', '{');
+    map.put(')', '(');
+    map.put(']', '[');
+    map.put('}', '{');
 
-        for(char c : s.toCharArray()){
-            if(map.containsValue(c)){
-                pile.push(c);
-            }else if(map.containsKey(c)){
+    for( char c : s.toCharArray()){
+        if(map.containsValue(c)){
+            pile.push(c);
+        }else{
+            if(map.containsKey(c)){
                 if(pile.isEmpty() || pile.pop() != map.get(c)){
                     return false;
                 }
+
             }
         }
+    }
         return pile.isEmpty();
-      
-        }
+    }
 
 
 
