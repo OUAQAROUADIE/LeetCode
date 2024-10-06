3b190106b61ff9a5ad52b3023113762dc3767511
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-10-06 21:22:07 +0100

    Time: 8 ms (87.43%), Space: 46 MB (98.03%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
 
-
         List<int[]> result = new ArrayList<>();
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
@@ -9,10 +8,12 @@ class Solution {
             if(result.isEmpty() || result.get(result.size()-1)[1] < inter[0]){
                 result.add(inter);
             }else{
-                result.get(result.size()-1)[1] = Math.max(result.get(result.size()-1)[1], inter[1]);
+                result.get(result.size()-1)[1] =Math.max(result.get(result.size()-1)[1], inter[1]);
             }
         }
         return result.toArray(new int[result.size()][]);
+
+        
         
     }
 }
