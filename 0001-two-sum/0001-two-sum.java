8060d7d18d210401155c3bd708af9d99c8c2a06f
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-11-02 18:12:57 +0100

    Time: 48 ms (13.85%), Space: 44.6 MB (76.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] tmp = new int[2];
+      int[] tmp = new int[2];
      for(int i = 0 ; i < nums.length-1; i++){
         int j = i+1;
         int r = target- nums[i] ;
@@ -16,5 +16,5 @@ class Solution {
         
      }
 return tmp;
-     }
+    }
 }
