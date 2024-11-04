3cec48aa6156ff8c704298162ef545448f3bcfc5
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-11-04 15:13:28 +0100

    Time: 16 ms (43.81%), Space: 42.4 MB (55.06%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length() != t.length()){return false;}
-        Map<Character,Integer> c1 = new HashMap<>();
-        Map<Character,Integer> c2 = new HashMap<>();
-        for(int i = 0 ; i < s.length();i++){
-            if(!c1.containsKey(s.charAt(i))){
-                c1.put(s.charAt(i),i);}
-            if(!c2.containsKey(t.charAt(i))){
-                c2.put(t.charAt(i),i);
-            }
+        Map<Character,Integer> map1 = new HashMap<>();
+        Map<Character,Integer> map2 = new HashMap<>();
 
-            if(!c1.get(s.charAt(i)).equals(c2.get(t.charAt(i)))){
+        for(int i = 0 ; i < s.length() ; i++){
+            if(!map1.containsKey(s.charAt(i))){
+                map1.put(s.charAt(i),i);
+            }
+            if(!map2.containsKey(t.charAt(i))){
+                map2.put(t.charAt(i),i);
+            }
+            if(map1.get(s.charAt(i)) != map2.get(t.charAt(i))){
                 return false;
             }
-        }                 
-        return true;
+        }
+return true;
+
     }
 }
