9d8ce2d530dd70af9751231c51790e521064fd58
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-11-06 21:55:36 +0100

    Time: 10 ms (9.34%), Space: 41.6 MB (65.83%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,30 +1,26 @@
 class Solution {
     public int longestPalindrome(String s) {
-       Map<Character, Integer> map = new HashMap<>();
-        int count = 0;
-        
-        // Count the frequency of each character
-        for (int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+       boolean valid = false;
+        if(s.length() == 1){return 1;}
+        Map<Character, Integer> map = new HashMap<>();
+        if(map.size() == 1){return map.get(s.charAt(0));}
+        int count =0;
+        for(int i = 0 ; i < s.length() ; i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)+1);
         }
-        
-        boolean hasOddFrequency = false;
-        
-        // Add even frequencies completely to the palindrome
-        for (int n : map.values()) {
-            if (n % 2 == 0) {
-                count += n; // Even frequencies can be fully used
-            } else {
-                count += n - 1; // Use the even part of odd frequencies
-                hasOddFrequency = true; // Remember that we can place one odd character in the center
+
+        for(int n : map.values()){
+            if(n % 2 ==0){
+                count +=n;
+            }else{
+                count += n-1;
+                valid =true;
             }
         }
-        
-        // If there is at least one character with an odd frequency, we can place one in the center
-        if (hasOddFrequency) {
-            count++;
+            if(valid){
+                count++;
+            
         }
-        
         return count;
     
     
