5a837cef962afc61e1aa6cec59454b2146471587
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-11-08 18:17:47 +0100

    Time: 0 ms (100%), Space: 42.3 MB (45.19%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,23 +1,20 @@
 class Solution {
-    public double myPow(double x, int n) {
-        if( n > 0){
-         return pow(x,n) ;
-        }
-        if( n < 0 && x != 0){
-            return 1 / (pow(x, n));
-        }
-        return 1;
+    public double myPow(double x, int n) { 
+        if( n < 0){
+            return 1/pow(x , n);
+        }  
+        return pow(x,n);  
     }
-
-    public double pow(double x , int n){
-        if( n == 0){return 1;}
-        double a = pow(x, n/2);
-        if( n % 2 == 0){
-            return a*a;
+    public double pow(double x, int n){
+        if( n == 0){
+            return 1;
+        }
+        double r = pow(x, n/2);
+        if(n % 2 == 0){
+            return r*r;
         }else{
-            return x*a*a;
-        } 
-        
+            return x*r*r;
+        }
     }
 }
 
