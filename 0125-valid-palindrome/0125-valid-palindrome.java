e2be65037ae471ff5e385b5f7cf61b5286a8632e
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-11-12 21:10:35 +0100

    Time: 3 ms (72.3%), Space: 43.6 MB (47.39%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,16 +1,13 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
-        for(char c : s.toCharArray()){
+       StringBuilder sb  =new StringBuilder();
+
+       for(char c : s.toCharArray()){
             if(Character.isLetterOrDigit(c)){
                 sb.append(Character.toLowerCase(c));
             }
-        }
+       }
 
-        if(sb.toString().equals(sb.reverse().toString())){
-            return true;
-        }
-
-        return false;
+       return sb.toString().equals(sb.reverse().toString());
     }
 }
