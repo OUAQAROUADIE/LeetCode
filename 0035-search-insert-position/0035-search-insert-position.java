445891a4b2d6ab5ea5cb1ad10ef2eebec004936a
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-12-28 23:07:57 +0100

    Time: 0 ms (100%), Space: 42.7 MB (80.35%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,10 +1,10 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int i = 0 ;
-        while (i < nums.length && nums[i] <= target) {
-            if(nums[i] == target){return i;}
-            i++;
-        }
-        return i;
+       int i = 0 ;
+       while(i  < nums.length && nums[i] <= target){
+        if(nums[i] == target){return i;}
+        i++;
+       }
+       return i;
     }
 }
