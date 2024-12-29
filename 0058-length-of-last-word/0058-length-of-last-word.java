a9231197b18d53adb28f937553cdb1fc20504eca
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-12-29 20:51:31 +0100

    Time: 0 ms (100%), Space: 41.9 MB (36.06%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,9 +1,8 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s= s.trim();
+       s = s.trim();
 
-        int lastindex = s.lastIndexOf(' ');
-
-        return s.length() - lastindex - 1;
+       int i = s.lastIndexOf(' ');
+       return s.length() - i -1;
     }
 }
