1b683940b9767cbe01671a81f8ec23ce59e52039
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-12-30 14:31:23 +0100

    Time: 158 ms (5.56%), Space: 44.7 MB (76.84%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,16 +1,15 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-
-        int i =  0;
-        for(int n : nums){
-            if(set.add(n)){
-                nums[i]=n;
-                i++;
-            }
+     Set<Integer> set = new HashSet<>();
+int i =0;
+     for(int n :nums){
+        if(set.add(n)){
+            nums[i]=n;
+            i++;
         }
-        return i;
-
+        
+     }
+     return i;
         
         
     }
