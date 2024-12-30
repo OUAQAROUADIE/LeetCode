19d1a111558e358f4859ddeaa2b3d82b7fc5890d
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-12-30 15:25:04 +0100

    Time: 1 ms (5.79%), Space: 41.9 MB (51.07%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,15 +1,17 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int  i = digits.length-1;
+        int i = digits.length -1;
         while(i >= 0){
             if(digits[i] == 9){
-                digits[i]=0;
+                digits[i] =0;
                 i--;
-            }else{
-                digits[i]= digits[i]+1;
+            }
+            else{
+                digits[i] +=1;
                 return digits;
             }
         }
-return IntStream.concat(IntStream.of(1), IntStream.of(digits)).toArray();
-    }
+            return IntStream.concat(IntStream.of(1), IntStream.of(digits)).toArray();
+        }
+    
 }
