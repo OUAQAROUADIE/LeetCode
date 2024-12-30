033ea1aa45c73a3cca24edbb5a1d46152ad09ce4
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-12-30 20:43:45 +0100

    Time: 2 ms (77.06%), Space: 62 MB (25.03%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -2,16 +2,13 @@ class Solution {
     public int maxProfit(int[] prices) {
         int max = 0;
         int min = Integer.MAX_VALUE;
-        int p = 0 ;
-        for(int i = 0 ; i < prices.length ; i++){
+        int p =0;
+        for(int i = 0 ; i  < prices.length ;i++){
             min = Math.min(min, prices[i]);
-            p = prices[i] - min;
+            p = prices[i]-min;
             max = Math.max(max, p);
-            
-
-        
         }
-          
-          return max;
+
+        return max;
     }
 }
