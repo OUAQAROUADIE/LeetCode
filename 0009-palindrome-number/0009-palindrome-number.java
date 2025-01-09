5ece19169cccdabef9a16d9809fc7719b93e15c4
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2025-01-09 22:52:49 +0100

    Time: 5 ms (71.86%), Space: 43.7 MB (95.29%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean isPalindrome(int x) {
-    if(x < 0){return false;}
-       int r = 0 ;
-       int n = x;
-
-       while(x > 0){
-        int tmp = x % 10;
-        r = r*10 + tmp;
-        x = x/10;
-       }
-     return n == r;
+        int n = x;
+        int s = 0;
+        while(x>0){
+            int tmp = x % 10;
+            s = s*10 + tmp;
+            x = x/10;
+        }
+        return n == s;
     }
 }
